79b50a67481e37f92c7aa930021d81581f568965
wey <gsw@qq.com>
2021-07-12 11:46:12 +0800

    feat: 添加react模板

--- bin/constants.js
+++ bin/constants.js
@@ -2,11 +2,13 @@ module.exports = {
   choices: [
     'Vue 2.0',
     'taro',
-    'vite-ts-vue3'
+    'vite-ts-vue3',
+    'React'
   ],
   template: {
     'Vue 2.0': 'direct:https://github.com/lzxb/vue2-demo.git',
     'taro': 'direct:https://github.com/bozaigao/Taro-demo.git',
-    'vite-ts-vue3': 'direct:https://github.com/swey01/vite-ts-vue3.git'
+    'vite-ts-vue3': 'direct:https://github.com/swey01/vite-ts-vue3.git',
+    'React': 'direct:https://github.com/zkeyword/react-webpack-boilerplate.git'
   }
 }
